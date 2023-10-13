247f042d2f3193120ac936461ed24015e4b9c765
Polar stoat <polarstoat@users.noreply.github.com>
2023-10-13 13:15:15 +0100

    Add Trakt OAuth device authentication

---
+++ trakt.authenticate.js
@@ -0,0 +1,30 @@
+import 'dotenv/config';
+import Conf from 'conf';
+import Trakt from 'trakt.tv';
+
+import parentLogger from './logger.js';
+
+const logger = parentLogger.child({}, { msgPrefix: '[auth] ' });
+
+const config = new Conf({ projectName: 'films-watched' });
+
+const trakt = new Trakt({
+  client_id: process.env.TRAKT_CLIENT_ID,
+  client_secret: process.env.TRAKT_CLIENT_SECRET,
+});
+
+await trakt.get_codes().then((poll) => {
+  logger.debug('get_codes() response: %o', poll);
+
+  logger.info('Authenticate this device with the code %s at %s', poll.user_code, poll.verification_url);
+
+  return trakt.poll_access(poll);
+}).then((resp) => {
+  logger.debug('poll_access() response: %o', resp);
+
+  const token = trakt.export_token();
+
+  config.set('traktToken', token);
+
+  logger.info('Got token %s… (expires %s)', token.access_token.substring(0, 8), new Date(token.expires).toUTCString());
+});
